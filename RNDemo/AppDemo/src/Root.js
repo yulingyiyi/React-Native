eab91598d8e60dec3b5954f2f8542c49045e517d
yuling <yuling996235005@gmail.com>
2018-05-20 23:07:05 +0800

    add redux

--- RNDemo/AppDemo/src/Root.js
+++ RNDemo/AppDemo/src/Root.js
@@ -1,6 +1,23 @@
 /*
  * @Author: yuling 
  * @Date: 2018-05-20 22:33:23 
- * @Last Modified by:   yuling 
- * @Last Modified time: 2018-05-20 22:33:23 
+ * @Last Modified by: yuling
+ * @Last Modified time: 2018-05-20 23:06:29
  */
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import configureStore from "./store/ConfigureStore.js";
+import from './pages/login'
+const store = configureStore();
+
+export default class Root extends Component{
+
+    render(){
+        return ( 
+            <Provider store={store}>
+              
+            </Provider>
+
+        )
+    }
+}
